045bda7595e21be820c0339ac671ad1d8d73095b
nitish.govekar <nitishgovekar1992@gmail.com>
2022-06-12 21:45:00 +0530

    Address Service configuration committed

--- address-service/src/main/java/com/microservices/controller/AddresscController.java
+++ address-service/src/main/java/com/microservices/controller/AddresscController.java
@@ -1,10 +1,34 @@
 package com.microservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.microservices.request.CreateAddressRequest;
+import com.microservices.response.AddressResponse;
+import com.microservices.service.AddressService;
 
 @RestController
 @RequestMapping("/api/address")
 public class AddresscController {
+	
+	@Autowired
+	AddressService addressService;
+	
+	@PostMapping("/create")
+	public AddressResponse createAddress(@RequestBody CreateAddressRequest createRequest) {
+		
+		return addressService.createAddress(createRequest);
+	}
+	
+	@GetMapping("/getById/{id}")
+	public AddressResponse getById(@PathVariable long id) {
+		return addressService.getById(id);
+		
+	}
 
 }
--- address-service/src/main/java/com/microservices/service/AddressService.java
+++ address-service/src/main/java/com/microservices/service/AddressService.java
@@ -1,8 +1,35 @@
 package com.microservices.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.microservices.entity.Address;
+import com.microservices.repository.AddressRepository;
+import com.microservices.request.CreateAddressRequest;
+import com.microservices.response.AddressResponse;
 
 @Service
 public class AddressService {
+	
+	@Autowired
+	AddressRepository addressRepository;
+	
+
+	public AddressResponse createAddress(CreateAddressRequest request) {
+		
+		Address address = new Address();
+		
+		address.setCity(request.getCity());
+		address.setStreet(request.getStreet());
+		addressRepository.save(address);
+		return new AddressResponse(address);
+	}
+
+	public AddressResponse getById(long id) {
+		
+		Address address = addressRepository.findById(id).get();
+		
+		return new AddressResponse(address);
+	}
 
 }
