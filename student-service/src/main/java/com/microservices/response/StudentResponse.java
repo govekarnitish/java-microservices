cd6fb83316bbae5081e8dfb12b8dcc6b0e53f54a
nitish.govekar <nitishgovekar1992@gmail.com>
2022-06-13 08:34:32 +0530

    Updated code to create template for Student Microservice

---
+++ student-service/src/main/java/com/microservices/response/StudentResponse.java
@@ -0,0 +1,56 @@
+package com.microservices.response;
+
+import com.microservices.entity.Student;
+
+public class StudentResponse {
+
+
+	private long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;	
+
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	public StudentResponse(Student student) {
+		this.id = student.getId();
+		this.firstName = student.getFirstName();
+		this.lastName = student.getLastName();
+		this.email = student.getEmail();	
+	}
+}
---
+++ student-service/src/main/java/com/microservices/service/StudentService.java
@@ -0,0 +1,38 @@
+package com.microservices.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.microservices.entity.Student;
+import com.microservices.repository.StudentRepository;
+import com.microservices.request.CreateStudentRequest;
+import com.microservices.response.StudentResponse;
+
+@Service
+public class StudentService {
+	
+	@Autowired
+	StudentRepository studentRepository;
+	
+
+	public StudentResponse createStudent(CreateStudentRequest request) {
+		
+		Student student = new Student();
+		student.setFirstName(request.getFirstName());
+		student.setLastName(request.getLastName());
+		student.setAddressId(request.getAddressId());
+		student.setEmail(request.getEmail());
+		
+		student = 	studentRepository.save(student);
+		
+		return new StudentResponse(student);
+	}
+
+	public StudentResponse getById(long id) {
+		
+		Student student = studentRepository.findById(id).get();
+		
+		return new StudentResponse(student);
+	}
+
+}
