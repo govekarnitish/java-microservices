11dfd409cccafba8abe32bdc86429fe2858f4902
nitish.govekar <nitishgovekar1992@gmail.com>
2022-06-21 15:06:09 +0530

    Updated code to comment out WebClient

--- student-service/src/main/java/com/microservices/service/StudentService.java
+++ student-service/src/main/java/com/microservices/service/StudentService.java
@@ -2,16 +2,12 @@ package com.microservices.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import com.microservices.entity.Student;
 import com.microservices.feignclients.AddressFeignClient;
 import com.microservices.repository.StudentRepository;
 import com.microservices.request.CreateStudentRequest;
-import com.microservices.response.AddressResponse;
 import com.microservices.response.StudentResponse;
-
-import reactor.core.publisher.Mono;
 
 @Service
 public class StudentService {
@@ -19,8 +15,9 @@ public class StudentService {
 	@Autowired
 	StudentRepository studentRepository;
 	
-	@Autowired
-	WebClient webClient;
+	/*
+	 * @Autowired WebClient webClient;
+	 */
 	
 	@Autowired
 	AddressFeignClient addressFeignClient;
@@ -48,10 +45,12 @@ public class StudentService {
 		return studentResponse;
 	}
 	
-	public AddressResponse getAddressById(long addressId) {
+/**	 Example of Webclient
+ public AddressResponse getAddressById(long addressId) {
+ 
 		Mono<AddressResponse> addressResponse = webClient.get().uri("/getById/"+ addressId).retrieve().bodyToMono(AddressResponse.class);
 		
 		return addressResponse.block();
 	}
-
+**/
 }
