1cdd5c5dff4d55d034b6aaefebc59a4d6d0a058a
nitish.govekar <nitishgovekar1992@gmail.com>
2022-06-21 15:54:39 +0530

    Updated code to configure feign client with Eureka Server

--- student-service/src/main/java/com/microservices/feignclients/AddressFeignClient.java
+++ student-service/src/main/java/com/microservices/feignclients/AddressFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.microservices.response.AddressResponse;
 
-@FeignClient(url = "${address.service.url}", value = "address-feign-client", path = "/api/address")
+@FeignClient(value = "address-service", path = "/api/address")
 public interface AddressFeignClient {
 
 	@GetMapping("/getById/{id}")
