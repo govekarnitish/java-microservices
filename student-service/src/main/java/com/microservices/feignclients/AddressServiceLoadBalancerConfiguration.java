1ad10ccdfd9f081030dad3643219960b2cff3aba
nitish.govekar <nitishgovekar1992@gmail.com>
2022-06-21 16:52:17 +0530

    Updated code to configure load balancer for Address Service

---
+++ student-service/src/main/java/com/microservices/feignclients/AddressServiceLoadBalancerConfiguration.java
@@ -0,0 +1,18 @@
+package com.microservices.feignclients;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+
+import feign.Feign;
+
+@LoadBalancerClient(value = "address-service")
+public class AddressServiceLoadBalancerConfiguration {
+
+	@LoadBalanced
+	@Bean
+	public Feign.Builder feignBuilder(){	
+		return  Feign.builder();
+
+	}
+}
